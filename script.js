41c98cb66154de2cb46c740771ae83e5e9c243ec
AlphaAcces <ops@blackbox.codes>
2025-06-20 03:43:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
+let i = 0;
+let text = "GREYEUE AI: Authenticating user...";
+let speed = 40; // millisekunder mellem hver karakter
+
+function typeWriter(targetElementId) {
+  if (i < text.length) {
+    document.getElementById(targetElementId).innerHTML += text.charAt(i);
+    i++;
+    setTimeout(() => typeWriter(targetElementId), speed);
+  }
+}
 
